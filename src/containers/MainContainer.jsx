83bd0c7d8daca183e3a89fd211ae27b568449dd5
rondido <rppr001@naver.com>
2023-06-09 02:07:05 +0900

    [design/Main] design: 메인 페이지 디자인

--- src/containers/MainContainer.jsx
+++ src/containers/MainContainer.jsx
@@ -1,10 +1,41 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+	display: flex;
+	justify-content: ceter;
+	flex-direction: column;
+	align-items: center;
+`;
+
+const ButtonItemDiv = styled.div`
+	display: flex;
+	justify-content: center;
+	flex-direction: column;
+	align-items: center;
+	width: 600px;
+	height: 500px;
+`;
+
+const Button = styled.button`
+	border: 1px solid blue;
+	border-radius: 5px;
+	background-color: transparent;
+	width: 200px;
+	height: 100px;
+
+	&:hover {
+		background-color: aliceblue;
+	}
+`;
 
 export default function MainContainer() {
 	return (
-		<>
-			<h3>원티드 프리온보딩 인턴십 선발 과제</h3>
-			<button>시작하기</button>
-		</>
+		<Container>
+			<ButtonItemDiv>
+				<h1>원티드 프리온보딩 인턴십 선발 과제</h1>
+				<Button>시작하기</Button>
+			</ButtonItemDiv>
+		</Container>
 	);
 }
