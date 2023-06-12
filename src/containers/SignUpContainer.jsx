708db706217e2a266f573d7993457daa19f83563
rondido <rppr001@naver.com>
2023-06-13 00:53:37 +0900

    [feat/signuppassword] feat: 회원가입 아이디 비밀번호 검증

--- src/containers/SignUpContainer.jsx
+++ src/containers/SignUpContainer.jsx
@@ -1,16 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import useValidation from 'lib/useValidation';
 
 export default function SignUpContainer() {
+	const [inputValid, setInputValid] = useState({
+		email: '',
+		password: '',
+	});
+
+	const inputChange = e => {
+		const { name, value } = e.target;
+		setInputValid({
+			...inputValid,
+			[name]: value,
+		});
+	};
+
+	const [emailStatus, passwordStatus] = useValidation(inputValid);
+
 	return (
 		<>
 			<h1>회원가입</h1>
 			<label>
-				이메일 <input data-testid="email-input" />
+				이메일 <input data-testid="email-input" name="email" onChange={inputChange} />
+				{emailStatus.message}
 			</label>
 			<label>
-				비밀번호 <input data-testid="password-input" />
+				비밀번호{' '}
+				<input
+					data-testid="password-input"
+					name="password"
+					type="password"
+					onChange={inputChange}
+				/>
+				{passwordStatus.message}
 			</label>
-			<button data-testid="signup-button">회원가입</button>
+			<button data-testid="signup-button" disabled={!emailStatus.status || !passwordStatus.status}>
+				회원가입
+			</button>
 		</>
 	);
 }
