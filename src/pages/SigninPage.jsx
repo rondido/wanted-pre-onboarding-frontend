d8df78f77c58ae999d81671b8380ee5422c483cd
rondido <rppr001@naver.com>
2023-06-16 19:07:04 +0900

    [refac/signview] refac: 로그인 컴포넌트 view와 container로 분리

--- src/pages/SigninPage.jsx
+++ src/pages/SigninPage.jsx
@@ -1,6 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+
 import SignInContainer from 'containers/SigninContainer';
+import { hasAccessToken } from 'tokens/token';
 
 export default function SigninPage() {
+	const navigate = useNavigate();
+	useEffect(() => {
+		if (!hasAccessToken()) {
+			navigate('/todo');
+			return;
+		}
+	}, []);
 	return <SignInContainer />;
 }
