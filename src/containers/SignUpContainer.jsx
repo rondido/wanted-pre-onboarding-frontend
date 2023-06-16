819a8e8779c0532cee6aa2b26c49f7ed40e368a1
rondido <rppr001@naver.com>
2023-06-16 21:09:01 +0900

    [refac/signupview] refac: 회원가입 container 컴포넌트 분리

--- src/containers/SignUpContainer.jsx
+++ src/containers/SignUpContainer.jsx
@@ -1,79 +1,10 @@
 import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router';
 
 import useValidation from 'lib/useValidation';
-import styled from 'styled-components';
 import { requerstSignUp } from 'apis/Api';
-import { useNavigate } from 'react-router';
-import { USER_ACCESS_TOKEN, hasAccessToken } from 'tokens/token';
-
-const Container = styled.div`
-	display: flex;
-	justify-content: center;
-	align-items: center;
-	font-family: Georgia, 'Times New Roman', serif;
-`;
-
-const Item = styled.div`
-	width: 500px;
-	height: 500px;
-	position: absolute;
-	top: 50%;
-	left: 50%;
-	transform: translate(-50%, -50%);
-`;
-
-const TitleDiv = styled.div`
-	width: 100%;
-	display: flex;
-	justify-content: center;
-`;
-
-const EmailLabelDiv = styled.div`
-	width: 100%;
-	display: flex;
-	justify-content: center;
-	flex-direction: column;
-	align-items: center;
-`;
-
-const PasswordLabelDiv = styled.div`
-	width: 100%;
-	display: flex;
-	justify-content: center;
-	flex-direction: column;
-	align-items: center;
-`;
-
-const InputFiled = styled.input`
-	width: 300px;
-	outline: none;
-	border-radius: 10px;
-	background-color: rgb(233, 233, 233);
-	border: 0;
-	height: 30px;
-`;
-
-const StatusMessage = styled.p`
-	color: red;
-`;
-
-const SignupButtonDiv = styled.div`
-	width: 100%;
-	display: flex;
-	justify-content: center;
-	flex-direction: column;
-	align-items: center;
-`;
-
-const SigupButton = styled.button`
-	width: 400px;
-	outline: none;
-	border-radius: 10px;
-	background-color: rgb(233, 233, 233);
-	border: 0;
-	height: 50px;
-	margin-bottom: 20px;
-`;
+import { hasAccessToken } from 'tokens/token';
+import SignupView from 'components/views/signup/SignupView';
 
 export default function SignUpContainer() {
 	const [signUpFormData, setSignUpFormData] = useState({
@@ -110,41 +41,12 @@ export default function SignUpContainer() {
 	};
 
 	return (
-		<Container>
-			<Item>
-				<TitleDiv>
-					<h1>회원가입</h1>
-				</TitleDiv>
-				<form onSubmit={handleSignUp}>
-					<EmailLabelDiv>
-						<label>
-							이메일 <InputFiled data-testid="email-input" name="email" onChange={inputChange} />
-						</label>
-						<StatusMessage>{emailStatus.message}</StatusMessage>
-					</EmailLabelDiv>
-					<PasswordLabelDiv>
-						<label>
-							비밀번호
-							<InputFiled
-								data-testid="password-input"
-								name="password"
-								type="password"
-								onChange={inputChange}
-							/>
-						</label>
-						<StatusMessage>{passwordStatus.message}</StatusMessage>
-					</PasswordLabelDiv>
-					<SignupButtonDiv>
-						<SigupButton
-							data-testid="signup-button"
-							disabled={!emailStatus.status || !passwordStatus.status}
-						>
-							회원가입
-						</SigupButton>
-						<SigupButton onClick={singinNavigate}>로그인</SigupButton>
-					</SignupButtonDiv>
-				</form>
-			</Item>
-		</Container>
+		<SignupView
+			inputChange={inputChange}
+			passwordStatus={passwordStatus}
+			emailStatus={emailStatus}
+			singinNavigate={singinNavigate}
+			handleSignUp={handleSignUp}
+		/>
 	);
 }
